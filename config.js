ce17a684c0ef2b6c46a7469a7ffabb41ce3a431c
Shigetoshi <shigetoshi04@gmail.com>
2023-09-21 01:33:20 +0400

    Update config.js

--- config.js
+++ config.js
@@ -71,9 +71,9 @@ const CONFIG = {
 		},
 		{
 			id: '4',
-			name: 'Calendar',
-			icon: 'calendar',
-			link: 'https://calendar.google.com/calendar/r',
+			name: 'Twitch',
+			icon: 'twitch',
+			link: 'https://www.twitch.tv/',
 		},
 		{
 			id: '5',
