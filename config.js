9ccb77fbea5689b9bc39da3f4a05423566b04e40
Shigetoshi <shigetoshi04@gmail.com>
2023-09-21 01:29:37 +0400

    Update config.js

--- config.js
+++ config.js
@@ -65,9 +65,9 @@ const CONFIG = {
 		},
 		{
 			id: '3',
-			name: 'Todoist',
-			icon: 'trello',
-			link: 'https://todoist.com',
+			name: 'vk',
+			icon: 'cat',
+			link: 'https://vk.com/im',
 		},
 		{
 			id: '4',
